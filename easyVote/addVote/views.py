a1b28f80dfe0409f1de449ef907714213fd360da
RedShark353 <poponelson@outlook.com>
2021-12-17 13:04:11 +0200

    remove code generator

--- easyVote/addVote/views.py
+++ easyVote/addVote/views.py
@@ -7,7 +7,11 @@ from .models import Candidate, Code
 
 # Create your views here.
 def index(request):
-    createCodes()
+    allCodes = Code.objects.all()
+    codeNames = []
+    for i in allCodes:
+        codeNames.append(i.code)
+    print(codeNames)
     return render(request, "addVote/newVote.html", {
         "candidates":Candidate.objects.all()
     })
